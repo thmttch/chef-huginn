2185fce0e5c727e4659f2fa0815209770e0e5cd3
Matt Chu <matt.chu@gmail.com>
2014-11-18 17:10:32 -0800

    wip

--- attributes/default.rb
+++ attributes/default.rb
@@ -4,9 +4,10 @@ default.huginn = {
   home: '/home/huginn',
 
   install_dir: '/home/huginn/huginn',
-  #repo_url: 'git://github.com/thmttch/huginn.git',
-  repo_url: 'https://github.com/cantino/huginn.git',
-  repo_branch: 'master',
+  #repo_url: 'https://github.com/cantino/huginn.git',
+  #repo_branch: 'master',
+  repo_url: 'git://github.com/thmttch/huginn.git',
+  repo_branch: 'thmttch',
 
   ruby_version: '2.1.5',
 
